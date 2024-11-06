99836b0d96c3264db98c025faa2cb8f3ad062743
“BiGMarK8” <markg981@gmail.com>
2024-11-06 13:54:17 +0000

    add code updates

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,24 +1,54 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
-public static int Arg1;
-    public static void main(String args[])    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+public class Dec2Hex {
+    public static void main(String[] args) {
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // ask user to input interger
+        System.out.print("Please enter an integer to convert to hexadecimal: ");
+        // store num
+        int num;
 
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+        // Check input is an integer
+        while (true) {
+            try {
+                num = Integer.parseInt(scanner.nextLine());
+                // Exit the loop if parsing is successful
+                break;
+            }
+            // if input is not integer
+            catch (NumberFormatException e) {
+                // display error message
+                System.err.print("Invalid input. Please enter a valid integer: ");
+            }
+        }
+        // close scanner
+        scanner.close();
+
+        // if input is 0
+        if (num == 0) {
+            // print message
+            System.out.println("Hexadecimal representation is: 0");
+            return;
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        // declare values
+        char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        String hexadecimal = "";
+        // print what number the program is converting
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
+        // Convert decimal to hexadecimal
+        // when number is not equal to 0
+        while (num != 0) {
+            // calculate remainder when num is divided by 16
+            int rem = num % 16;
+            // hexadecimal equals matching rem to hexchars character
+            hexadecimal = hexChars[rem] + hexadecimal;
+            // update num
+            num = num / 16;
+        }
+        // print the hexadecimal value
+        System.out.println("Hexadecimal representation is: " + hexadecimal);
     }
 }
